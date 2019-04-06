4867e3b6574560624c9eb87d34bb9ada8951baa0
Annu Shokeen <shokeen.annu@gmail.com>
2019-04-06 18:27:54 +1100

    changes

--- MonashPal/App_Start/RouteConfig.cs
+++ MonashPal/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MonashPal
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "SearchEngine", action = "SearchHome", id = UrlParameter.Optional }
             );
         }
     }
